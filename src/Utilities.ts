19fd962a7a6a59508e682d658cf2230c7a5abf9e
Russell Richardson <Russell@KronosAD.com>
2021-10-07 00:51:40 -0400

    Started creating RoleType which will deprecate PermissionLevel later on!
    RoleTypes are connected to roles in Discord which allow for more granular permissions to commands.

--- src/Utilities.ts
+++ src/Utilities.ts
@@ -1,12 +1,100 @@
 import * as Discord from 'discord.js'
-import { PermissionLevel } from './types';
+import { PermissionLevel, RoleType } from './types';
 import * as Firestore from '@google-cloud/firestore';
+import { firestore } from 'firebase';
 
 export class Utilities {
+
+    private static db = new Firestore.Firestore({
+        projectId: process.env.FIREBASE_PROJECT_ID,
+        keyFilename: 'firebase-auth.json'
+    })
+
     static getUserPermissionLevel(user: Discord.GuildMember, db: Firestore.Firestore): PermissionLevel {
         if(user.hasPermission("ADMINISTRATOR")) {
             return PermissionLevel.ADMIN
         }
         return PermissionLevel.EVERYONE
     }
+
+    static async setRoleAsType(guild: Discord.Snowflake, type: RoleType, roleID: String){
+        var guildConfigRef = Utilities.db.doc(`guilds/${guild}`)
+
+        if(!(await guildConfigRef.get()).exists) {
+            await guildConfigRef.create({})
+        }
+
+        switch(type) {
+            default:
+                console.error(`Could not resolve role type from ${type}`)
+                throw new Error(`Could not resolve role type from ${type}`)
+            case RoleType.EVERYONE:
+                await guildConfigRef.set({ "EVERYONE": roleID }, { merge: true })
+                break
+            case RoleType.SOFT_TRUSTED:
+                await guildConfigRef.set({ "SOFT_TRUSTED": roleID }, { merge: true })
+                break
+            case RoleType.MAX_TRUSTED:
+                await guildConfigRef.set({ "MAX_TRUSTED": roleID }, { merge: true })
+                break
+            case RoleType.MODERATOR:
+                await guildConfigRef.set({ "MODERATOR": roleID }, { merge: true })
+                break
+            case RoleType.ADMIN:
+                await guildConfigRef.set({ "ADMIN": roleID }, { merge: true })
+                break
+            case RoleType.DEVELOPER:
+                await guildConfigRef.set({ "DEVELOPER": roleID }, { merge: true })
+                break
+        }        
+        
+    }
+
+    static async getRoleFromType(guild: Discord.Guild, type: RoleType): Promise<String> {
+        var guildConfigRef = Utilities.db.doc(`guilds/${guild.id}`)
+
+        if(!(await guildConfigRef.get()).exists) {
+            return "NONE"
+        }
+
+        var roleData = (await guildConfigRef.get()).data() as unknown as RoleTypeStructure
+
+        var everyoneRoleId = guild.roles.everyone.id
+
+        switch(type) {
+            case RoleType.EVERYONE:
+                return (roleData.EVERYONE == undefined ? everyoneRoleId : roleData.EVERYONE)
+            case RoleType.SOFT_TRUSTED:
+                return roleData.SOFT_TRUSTED
+            case RoleType.MAX_TRUSTED:
+                return roleData.MAX_TRUSTED
+            case RoleType.MODERATOR:
+                return roleData.MODERATOR
+            case RoleType.ADMIN:
+                return roleData.ADMIN
+            case RoleType.DEVELOPER:
+                return roleData.DEVELOPER
+        }
+    }
+
+    static async doesUserHaveRoleType(guild: Discord.Guild, type: RoleType, user: Discord.GuildMember): Promise<boolean> {
+        var minRoleId = await this.getRoleFromType(guild, type)
+        var role = guild.roles.cache.find(r => r.id == minRoleId)
+
+        if(role == undefined) {
+            return false
+        }
+
+        return user.roles.highest.comparePositionTo(role) >= 0
+    }
+    
+}
+
+interface RoleTypeStructure {
+    EVERYONE: String
+    SOFT_TRUSTED: String
+    MAX_TRUSTED: String
+    MODERATOR: String
+    ADMIN: String
+    DEVELOPER: String
 }
--- src/types.ts
+++ src/types.ts
@@ -1,6 +1,17 @@
-export enum PermissionLevel {
+export const enum PermissionLevel {
     EVERYONE = 1,
     MODERATOR = 2,
     ADMIN = 3,
-    DEVELOPER = 4
+    DEVELOPER = 4,
+    CUSTOM = 5
+}
+
+export enum RoleType {
+    EVERYONE = "EVERYONE",
+    SOFT_TRUSTED = "SOFT_TRUSTED",
+    MAX_TRUSTED = "MAX_TRUSTED",
+    MODERATOR = "MODERATOR",
+    ADMIN = "ADMIN",
+    DEVELOPER = "DEVELOPER"
+
 }
